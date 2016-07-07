f45df1d3b799dfd0e3a1b5b8be1b5c5b9002963b
akash-sharma <akash777.sharma@gmail.com>
2016-07-08 01:14:46 +0530

    download file code via spring mvc

--- src/main/java/com/akash/controller/DashboardController.java
+++ src/main/java/com/akash/controller/DashboardController.java
@@ -1,10 +1,15 @@
 package com.akash.controller;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -25,7 +30,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.akash.constant.Gender;
-import com.akash.hibernate.utils.HibernateUtils;
 import com.akash.model.domain.Person;
 import com.akash.model.domain.validator.PersonValidator;
 import com.akash.service.PersonService;
@@ -57,8 +61,7 @@ public class DashboardController {
 	private boolean isValidFile;
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String homePage(HttpServletRequest req, HttpServletResponse res,
-			Model model) {
+	public String homePage(HttpServletRequest req, HttpServletResponse res, Model model) {
 		logger.debug("index page:" + isValidExample);
 		List listOfUsers = new ArrayList();
 		for (int i = 0; i < 5; i++) {
@@ -91,14 +94,13 @@ public class DashboardController {
 	}
 
 	@RequestMapping(value = "/savePerson", method = RequestMethod.POST)
-	public String savePerson(HttpServletRequest request, @Validated Person person, BindingResult result,
-			Model model) {
+	public String savePerson(HttpServletRequest request, @Validated Person person, BindingResult result, Model model) {
 		if (!result.hasErrors()) {
 			if (person.getVersion() == null) {
 				personService.save(person);
 			} else {
 				logger.error("XXXXXXXXXXX before merge XXXXXXXXXXXXXX");
-//				personService.merge(person);
+				// personService.merge(person);
 				personService.update(person);
 				logger.error("XXXXXXXXXXX after merge XXXXXXXXXXXXXX");
 			}
@@ -113,27 +115,27 @@ public class DashboardController {
 		return personService.getAllPerson();
 	}
 
-	//NOTE : both load() and get() fires 2 select queries
-	//i.e. get with get , load with load, get with load
+	// NOTE : both load() and get() fires 2 select queries
+	// i.e. get with get , load with load, get with load
 	@RequestMapping(value = "/editPerson/{personId}", method = RequestMethod.GET)
 	public String editPerson(@PathVariable String personId, Model model) {
 
 		logger.error("<<<<<<<<<<<<<<<<<<queries starting here>>>>>>>>>>>>>>>>>>>>>>>>");
 		Person person1 = personService.getPersonById(personId);
-//		Person person2 = personService.get(personId);
+		// Person person2 = personService.get(personId);
 		Person person3 = personService.get(personId);
-//		Person person4 = personService.load(personId);
+		// Person person4 = personService.load(personId);
 		Person person5 = personService.load(personId);
 		logger.error("<<<<<<<<<<<<<<<<<<queries ending here>>>>>>>>>>>>>>>>>>>>>>>>");
 		model.addAttribute("person", person5);
 		return "editPerson";
 	}
-	
+
 	@RequestMapping(value = "/test", method = RequestMethod.GET)
 	@ResponseBody
 	@Transactional
 	public String testTransaction() {
-		
+
 		Person person = new Person();
 		person.setAge(20);
 		person.setName("random");
@@ -142,7 +144,57 @@ public class DashboardController {
 		personService.save(person);
 		try {
 			personService.updateName("5871667e-c678-444a-b6b6-afd21e336e11", "aaaaaaa");
-		}catch(Exception e) {}
+		} catch (Exception e) {
+		}
 		return "true";
+	}
+
+	// http://www.codejava.net/frameworks/spring/spring-mvc-sample-application-for-downloading-files
+
+	@RequestMapping(method = RequestMethod.GET, value = "/download")
+	public void doDownload(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+		Date startTime = new Date();
+		ServletContext context = request.getSession().getServletContext();
+
+		String fileName = new Date().getTime() + ".csv";
+
+		// get MIME type of the file
+		String mimeType = context.getMimeType(fileName);
+		if (mimeType == null) {
+			// set to binary type if MIME mapping not found
+			mimeType = "application/octet-stream";
+		}
+		System.out.println("MIME type: " + mimeType);
+
+		// set content attributes for the response
+		response.setContentType(mimeType);
+
+		// set headers for the response
+		String headerKey = "Content-Disposition";
+		String headerValue = String.format("attachment; filename=\"%s\"", fileName);
+		response.setHeader(headerKey, headerValue);
+		
+		
+		// get output stream of the response
+		OutputStream outStream = response.getOutputStream();
+		String string = getCsvContent(outStream);
+//		outStream.write(string.getBytes(Charset.forName("UTF-8")));
+		outStream.close();
+		
+		
+		Date endTime = new Date();
+		System.out.println("time taken : " + (endTime.getTime() - startTime.getTime()));
+	}
+
+	private String getCsvContent(OutputStream outStream) throws IOException {
+
+		String str = "RefID_1466673403019,bbbbbbbbbbbbbbbb,ccccccccccccccc,01/20/1990,01/20/1990,RefID_1466673403019,01/20/1990\n";
+		StringBuilder content = new StringBuilder();
+		for (int i = 0; i < 6000000; i++) {
+			outStream.write(str.getBytes(Charset.forName("UTF-8")));
+//			content.append(str);
+		}
+		return content.toString();
 	}
 }
